a9a398636377afe8e32879533688ec0cb05d77f2
202101215K <202101215k@utea.edu.pe>
2024-11-20 07:49:20 -0500

    tipos comic

---
+++ variables3.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //Tipos dinamicos
+  //var y dynamic
+  var unaVariable = "Hola";
+  dynamic otraVariable = 1;
+
+  int numero = 15;
+
+  print(unaVariable);
+  print(otraVariable);
+  print(otraVariable + numero);
+}
