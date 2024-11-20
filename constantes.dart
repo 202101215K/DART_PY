2e00cd126f1d8d0d4f30264b5ef6154021fec2e7
202101215K <202101215k@utea.edu.pe>
2024-11-20 07:53:37 -0500

    constante final

---
+++ constantes.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final int constante = 12;
+  print(constante);
+  print(constante.toString());
+
+  final List<int> constante2 = [12,15];
+  constante2.add(18);
+  print(constante2);
+
+}
