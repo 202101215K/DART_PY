937737b2981896cb461281d410ba1180803e3a0c
202101215K <202101215k@utea.edu.pe>
2024-11-20 07:43:21 -0500

    variables1

---
+++ variables1.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //Cadenas de caracteres una cadena es un array
+
+  String miCadena = 'Hola soy Fernando';
+  print(miCadena);
+  print(miCadena.length);
+  print(miCadena.toLowerCase()); //Minuscula
+  print(miCadena.toUpperCase()); // Mayuscula
+
+  print(miCadena.substring(0,4));// Extrae las letras
+}
