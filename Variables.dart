aa286ba6e6573022a2c1a06823b8213df37eea4b
202101215K <202101215k@utea.edu.pe>
2024-11-20 07:34:46 -0500

    Variables

---
+++ Variables.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  //Los numeros solamente hay dos numeros entero y reales
+  //Numero entero
+  int miEntero = 2;
+  //reales
+  double miReal = 1.256;
+
+  //Enteros
+ print(miEntero);
+ print(miEntero.toString());
+ print(miEntero+miEntero);
+
+ //Reales
+ print(miReal.toInt());
+
+ //var ES UN TIPO DE DATO DEFINIDO POR QUE ES VARIADO
+ var x = 1;
+ var y = "2";
+
+ print(x);
+ print(y);
+ 
+}
