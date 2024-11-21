949238ecfd0362fd0f46a9ebc39c2b4b43855a79
202101215K <202101215k@utea.edu.pe>
2024-11-21 06:44:21 -0500

    listas

---
+++ listas.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  // Las listas son un conjunto
+  //Listas 
+  var listaNumeros = [1,2,3];
+
+ /* print(listaNumeros.length);
+  print(listaNumeros);
+  print(listaNumeros[1]);*/
+
+
+  List<int> listaNumeros1 = [2 , 5, 6, 9, 7];
+  print(listaNumeros1);
+  listaNumeros1.add(400); // agregar a la lista
+  print(listaNumeros1);
+  listaNumeros1.clear(); // Muestra la lista vacia
+  print(listaNumeros1);
+  print(listaNumeros1.indexOf(5)); // Buscar elementos en la lista
+
+  //listaNumeros1.forEach((x){
+   // print(x);
+ // });
+ print(listaNumeros1.reversed);
+
+  //Colecciones
+   
+}
