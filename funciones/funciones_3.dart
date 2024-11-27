48f20ca3ab2642ceeb1f23a2fbd404dfac03e9d6
202101215K <202101215k@utea.edu.pe>
2024-11-26 22:44:50 -0500

    actualizacion

--- funciones/funciones_3.dart
+++ funciones/funciones_3.dart
@@ -1,10 +1,12 @@
-void main(List<String> args) {
-  //Funciones anonimas: estas funciones no tienen un nombre y solo se puede utilizar solo una vez 
-  var nombre = ["Ruth", "Max","Fernando"];
-  nombre.forEach((item){
-    print("${nombre.indexOf(item)}: $item");
- //ejemplo de uso 2
-   //nombre.forEach((item) => print("${nombre.indexOf(item)}: $item"));
+void main(List<String> args){
+  // Funciones anónimas: Estas funciones no tienen un nombre y solo se pueden utilizar una vez.
+  var nombres = ["Ruth", "Max", "Fernando"];
 
-//ingresar el tamanio pequenio, mediano, familiar el programa debe calcular el precio 
+  // Uso de una función anónima en forEach
+  nombres.forEach((item) {
+    print("${nombres.indexOf(item)}: $item");
+  });
+
+  // Ejemplo de uso alternativo (forma más corta)
+  nombres.forEach((item) => print("${nombres.indexOf(item)}: $item"));
 }
